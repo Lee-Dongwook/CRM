6c0c1a0191c9ee237767f75fcae382d22cfce90d
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-02-20 16:43:53 +0900

    feat :schema

---
+++ actions/admin/update-gpt-model.ts
@@ -0,0 +1,22 @@
+"use server";
+
+import { prismadb } from "@/lib/prisma";
+
+const updateModel = async (model: any) => {
+  await prismadb.gpt_models.updateMany({
+    data: {
+      status: "INACTIVE",
+    },
+  });
+
+  const setCronGPT = await prismadb.gpt_models.update({
+    where: {
+      id: model,
+    },
+    data: {
+      status: "ACTIVE",
+    },
+  });
+};
+
+export default updateModel;
