55bbd1a0901d4047a1732d3aa99f45c69dc141ee
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-02-20 13:25:08 +0900

    feat : next-auth

---
+++ lib/notion.ts
@@ -0,0 +1,32 @@
+import { Client } from "@notionhq/client";
+import { prismadb } from "./prisma";
+import { error } from "console";
+
+const initNotionClient = async (userId: string) => {
+  try {
+    const apiKey = await prismadb.secondBrain_notions.findFirst({
+      where: {
+        user: userId,
+      },
+    });
+
+    if (!apiKey) {
+      const notionItems: any = {
+        error: "API key not found in the database",
+      };
+
+      return notionItems;
+    }
+
+    const notion = new Client({
+      auth: apiKey.notion_api_key,
+    });
+
+    return notion;
+  } catch (error) {
+    console.error("Failed to initialize notion client");
+    throw error;
+  }
+};
+
+export default initNotionClient;
